072fbb30e4422a23e678f254863da4a7859334fd
Ninjapiraatti <tuoppil@gmail.com>
2020-10-09 18:32:33 +0300

    Commented out the test part, gotta figure it out at some point

--- src/kilke/mod.rs
+++ src/kilke/mod.rs
@@ -1,3 +1,12 @@
 pub fn run() {
+    // This macro removes this inline module when Rust 
+    // is not in test mode.
+    //#[cfg(test)]
+    //mod tests {
+        // Notice that we don't immediately get access to the 
+        // parent module. We must be explicit.
+        //use super::*;
+        //... tests ...
+    //}
     println!("This is Kilke");
 }
