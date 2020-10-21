3e96cee870e3acaba399c704a704d048937f234d
Ninjapiraatti <tuoppil@gmail.com>
2020-10-21 08:38:58 +0300

    Solved casino chips

--- src/codewars_casinochips.rs
+++ src/codewars_casinochips.rs
@@ -28,46 +28,48 @@ mod tests {
         assert_eq!(solve(&[7, 4, 10]), 10);
         assert_eq!(solve(&[12, 12, 12]), 18);
         assert_eq!(solve(&[1, 23, 2]), 3);
-
-        // 4 2 1 > 3 1 1 > 2 0 1 > 1 0 0                                                   3 7
-        // 7 3 1 > 6 2 1 > 5 1 1 > 4 0 1 > 3 0 0                                           4 11 
-        // 9 8 2 > 8 7 2 > 7 6 2 > 6 5 2 > 5 4 2 > 4 3 2 > 3 2 2 > 2 1 2 > 1 1 1 > 0 0 1   9 19
-        // 5 5 0 > 4 4 0 > 3 3 0 > 2 2 0 > 1 1 0 > 0 0 0                                   5 10
-        // 8 8 2 > 7 7 2 > 6 6 2 > 5 5 2 > 4 4 2 > 3 3 2 > 2 1 2 > 1 1 1 > 0 0 1           8 18
-        // 6 3 1 > 5 2 1 > 4 1 1 > 3 1 0 > 2 0 0                                           4 10 
     }
 }
+
+// Pretty brute force. There must be an elegant mathematical solution.
 
 fn solve(arr: &[u32; 3]) -> u32 {
     let mut res = arr.to_vec();
     let mut days = 0;
     res.sort();
-    while res[0] >= 12000 {
-        res[0] -= 12000;
-        res[1] -= 12000;
-        res[2] -= 12000;
-        days += 18000;
+    while res[0] >= 10000 {
+        res[1] -= 1000;
+        res[2] -= 1000;
+        days += 1000;
+        res.sort();
     }
     while res[2] > 0 && res[1] > 0{
-        res.sort();
         res[2] -= 1;
         if res[1] > 0 {
             res[1] -= 1;
         } else if res[0] > 0 {
             res[0] -= 1;
         }
+        res.sort();
         days += 1;
     }
-    println!("{:?}", (res, days));
     days
 }
 
 pub fn run () {
-    solve(&[45, 100, 400]);
+    solve(&[24045, 24100, 24400]);
 }
 
 /* CODEWARS GOOD SOLUTIONS
 
-
+fn solve(arr: &[u32; 3]) -> u32 {
+    let mut chips = arr.to_vec();
+    chips.sort();
+    
+    match chips[0] + chips[1] < chips[2] {
+        true => chips[0] + chips[1],
+        false => (chips[0] + chips[1] + chips[2]) / 2,
+    }
+}
 
 */
