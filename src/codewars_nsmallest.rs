ab8e0fc5a4de3a5f1febf28dd564c403dfa57f99
ninjapiraatti <tuomas.louekari@planetoidi.com>
2021-02-20 19:26:22 +0200

    Solved n_smallest

--- src/codewars_nsmallest.rs
+++ src/codewars_nsmallest.rs
@@ -6,6 +6,8 @@
 // a lame bas sorting method there) with an array/list/vector of integers
 // and the expected number n of smallest elements to return.
 
+// Sort by tuple 2nd element: https://stackoverflow.com/questions/40091161/sorting-a-vector-of-tuples-needs-a-reference-for-the-second-value
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -27,17 +29,37 @@ mod tests {
 
 
 fn n_smallest(arr: &[i32], n: usize) -> Vec<i32> {
-    //let mut sorted = &arr;
-    let mut sorted = arr.to_vec();
-    sorted.sort();
-    println!("{:?}", sorted[0..n].to_vec());
-    sorted[0..n].to_vec()
+    let arrtovec = arr.to_vec();
+    let mut numbers = Vec::new(); // Has to be vec because array length must be known runtime
+    for (pos, e) in arrtovec.iter().enumerate() {
+        numbers.push((e, pos));
+    }
+    numbers.sort();
+    numbers = numbers[0..n].to_vec();
+    numbers.sort_by_key(|k| k.1);
+    let mut res = Vec::new();
+    for i in numbers {
+        res.push(*i.0);
+    }
+    res
 }
 
 pub fn run () {
-    n_smallest(&[0, 1, 1, 5, 6, 8, 3, 4, 10, -5], 2);
+    n_smallest(&[0, 1, 1, 5, 6, 8, 3, 4, 10, -5], 8);
 }
 
 /* CODEWARS SOLUTIONS
+
+use itertools::Itertools;
+
+fn first_n_smallest(arr: &[i32], n: usize) -> Vec<i32> {
+    arr.iter()
+        .enumerate()
+        .sorted_by_key(|(_, &c)| c)
+        .take(n)        
+        .sorted_by_key(|(i, _)| *i)
+        .map(|(_, &c)| c)
+        .collect()
+}
 
 */
