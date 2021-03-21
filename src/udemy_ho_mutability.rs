1a8e5c140b299e2e0adfa41ded85ec432a3e9d20
Ninjapiraatti <tuoppil@gmail.com>
2021-03-21 08:51:52 +0200

    Added new lesson

--- src/udemy_ho_mutability.rs
+++ src/udemy_ho_mutability.rs
@@ -1,6 +1,33 @@
 // Hands-On Data Structures and Algorithms in Rust
 // Video 7: Mutability, variables, copying and cloning
 
+#[derive(Debug, Clone)] // String doesn't implement copy so you can't use it here. Clone instead
+pub struct Aninmal {
+	name: String,
+	isfloof: bool,
+	noms: NomLocation
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct NomLocation {
+	x: i32,
+	y: i32
+}
+
+impl NomLocation {
+	pub fn new(x:i32, y:i32) -> Self {
+		NomLocation{x,y}
+	}
+}
+
 pub fn run () {
-	
+	let mut treefloof = Aninmal {
+		name: "Treefloof".to_string(),
+		isfloof: true,
+		noms: NomLocation::new(2, 4)
+	};
+
+	let tigger = treefloof.clone();
+	treefloof.name.push_str(" Tabernackle");
+	println!("{:?}, {:?}", treefloof, tigger);
 }
